a888aec14d66eb7a9d4bacec37bfc9f76afb24c8
Bryce Matsuda <iexist4jcore@gmail.com>
2016-02-17 21:35:53 -0800

    Add basic 'marshmallow' movement and camera following

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,11 +2,23 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	private Rigidbody rb;
+	public float speed;
+	
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
 	}
+	
+	// Update is called once per frame (before physics)
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
